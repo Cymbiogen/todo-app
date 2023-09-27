254498038a3d01380de05c47367c52514c62c9c4
PythonLearning <andreichifan87@gmail.com>
2023-09-27 09:18:43 +0300

    implemented add button

--- Convertor.py
+++ Convertor.py
@@ -1,5 +1,7 @@
 import PySimpleGUI as sg
 
+sg.theme("Black")
+
 feet_label = sg.Text("Enter Feet:")
 feet_input = sg.Input(key="feet")
 
@@ -9,18 +11,25 @@ inches_input = sg.Input(key="inches")
 convert_button = sg.Button("Convert")
 output_label = sg.Text(key="output")
 
+exit_button = sg.Button("Exit")
+
 window = sg.Window("Convertor", layout=[
                                     [feet_label, feet_input],
                                     [inches_label, inches_input],
-                                    [convert_button, output_label]])
+                                    [convert_button, exit_button, output_label]])
 while True:
     event, values = window.read()
     match event:
         case 'Convert':
-            feet = float(values["feet"])
-            inches = float(values['inches'])
-            meters = feet * 0.3048 + inches * 0.0254
-            window["output"].update(meters)
+            try:
+                feet = float(values["feet"])
+                inches = float(values['inches'])
+                meters = feet * 0.3048 + inches * 0.0254
+                window["output"].update(meters)
+            except ValueError:
+                sg.popup("Please insert values")
+        case "Exit":
+            break
         case sg.WINDOW_CLOSED:
             break
 
